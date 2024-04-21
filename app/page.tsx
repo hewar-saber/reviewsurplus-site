eb8b4268647109be2f8c69cc99f7e208fc41ad28
unknown <hewrchelsea@gmail.com>
2024-04-21 23:37:04 +0200

    Changed text

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Page() {
                 <div className={styles.text}>
                     <h1>Increase Revenue With More 5-Star Reviews.</h1>
                     <p>
-                        Increase your revenue by getting more 5-star reviews.
-                        Get notified when you receive new reviews on your
-                        business.
+                        Increase your revenue easily by collecting more 5-star
+                        reviews with our reputation management software. Book a
+                        call if you can handle more business.
                     </p>
                 </div>
                 <img
