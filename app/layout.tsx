0db2cba3f78c77cc05b9c4d9a8cb6b07969edb6b
unknown <hewrchelsea@gmail.com>
2024-04-22 13:42:08 +0200

    Added Facebook PX

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header/Header'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 const roboto = Roboto({ weight: '400', subsets: ['latin'] })
 
@@ -101,6 +102,30 @@ export default function RootLayout({
                 <meta name='application-name' content='Review Surplus' />
                 <meta name='msapplication-TileColor' content='#ff0000' />
                 <meta name='theme-color' content='#ffffff' />
+
+                <Script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        <script>
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '899628412174054');
+                        fbq('track', 'PageView');
+                        </script>
+                        <noscript><img height="1" width="1" style="display:none"
+                        src="https://www.facebook.com/tr?id=899628412174054&ev=PageView&noscript=1"
+                        /></noscript>
+                    `
+                    }}
+                    id='facebook-pixel'
+                    strategy='afterInteractive'
+                />
             </head>
             <body className={roboto.className}>
                 <Header />
